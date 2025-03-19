d77246edba986d09fabd35d52e15d0bf64a2b0b9
Edoardo Vacchi <evacchi@users.noreply.github.com>
2025-03-19 16:02:29 +0100

    wip
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- core/src/main/java/com/dylibso/mcpx4j/core/McpxServlet.java
+++ core/src/main/java/com/dylibso/mcpx4j/core/McpxServlet.java
@@ -1,48 +1,9 @@
 package com.dylibso.mcpx4j.core;
 
 import java.util.Map;
-import java.util.Objects;
 
-public class McpxServlet {
-    private final String name;
-    private final ServletInstall config;
-    private final Map<String, McpxTool> tools;
+public interface McpxServlet {
+    String name();
 
-    public McpxServlet(String name, ServletInstall config, Map<String, McpxTool> tools) {
-        this.name = name;
-        this.config = config;
-        this.tools = tools;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public ServletInstall config() {
-        return config;
-    }
-
-    public Map<String, McpxTool> tools() {
-        return tools;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof McpxServlet)) return false;
-        McpxServlet that = (McpxServlet) o;
-        return Objects.equals(name, that.name) && Objects.equals(tools, that.tools);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, tools);
-    }
-
-    @Override
-    public String toString() {
-        return "McpServlet{" +
-                "name='" + name + '\'' +
-                ", tools=" + tools +
-                '}';
-    }
+    Map<String, McpxTool> tools();
 }
